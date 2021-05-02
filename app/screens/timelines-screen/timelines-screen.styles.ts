9d8fdea6321da3806f21bfd8ebe741d890fcf45b
Andreas Olsson <noname.olsson@gmail.com>
2021-05-02 16:32:53 +0200

    fix: 🐛 Correct position of loader (#131)

--- app/screens/timelines-screen/timelines-screen.styles.ts
+++ app/screens/timelines-screen/timelines-screen.styles.ts
@@ -3,6 +3,7 @@ import { StyleSheet, ViewStyle } from "react-native"
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: "center",
   } as ViewStyle,
   emptyStateButtonWrapper: {
     flex: 1,
